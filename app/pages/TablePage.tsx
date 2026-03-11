bbe62cc4ac7835839d9edde8daa5e85d08c182d0
thomascswalker <thomascswalker@gmail.com>
2026-03-11 10:39:51 -0400

    sort of reorg

---
+++ app/pages/TablePage.tsx
@@ -0,0 +1,112 @@
+import Table from "@/app/components/Table/Table";
+import { Food, Nutrient } from "@/app/data/usda/types";
+import { fetchUsdaData } from "@/app/data/usda/usda";
+import {
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import React, { useEffect, useState } from "react";
+import { ScrollView, Text, View } from "react-native";
+
+const VISIBLE_NUTRIENTS = ["Energy", "Sucrose", "Fructose", "Lactose"];
+
+const columns = [
+  {
+    accessorKey: "fdcId",
+    header: "ID",
+    cell: ({ getValue }: { getValue: () => number }) => {
+      return <Text>{getValue()}</Text>;
+    },
+  },
+  {
+    accessorKey: "description",
+    header: "Description",
+    cell: ({ getValue }: { getValue: () => string }) => {
+      return <Text>{getValue()}</Text>;
+    },
+  },
+  {
+    accessorKey: "foodNutrients",
+    header: "Nutrients",
+    cell: ({ getValue }: { getValue: () => Nutrient[] }) => {
+      const nutrients = getValue();
+      return (
+        <View>
+          {nutrients
+            .filter(
+              (nutrient) =>
+                VISIBLE_NUTRIENTS.includes(nutrient.name) &&
+                nutrient.amount > 0,
+            )
+            .sort((n1, n2) => n1.name.localeCompare(n2.name))
+            .map((nutrient, index) => (
+              <Text key={index}>
+                {nutrient.name}: {nutrient.amount} {nutrient.unitName}
+              </Text>
+            ))}
+        </View>
+      );
+    },
+  },
+];
+
+function TablePage() {
+  const [data, setData] = useState<Food[]>([]);
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetchUsdaData("Foundation", 200, 1);
+      setData(result);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 20,
+      }}
+    >
+      <Table>
+        <Table.Head>
+          {table.getHeaderGroups().map((hg) => (
+            <Table.Row key={hg.id}>
+              {hg.headers.map((header) => (
+                <Table.Header key={header.id}>
+                  {flexRender(
+                    header.column.columnDef.header,
+                    header.getContext(),
+                  )}
+                </Table.Header>
+              ))}
+            </Table.Row>
+          ))}
+        </Table.Head>
+        <ScrollView>
+          <Table.Body>
+            {table.getRowModel().rows.map((row) => (
+              <Table.Row key={row.id}>
+                {row.getVisibleCells().map((cell) => (
+                  <Table.Cell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </Table.Cell>
+                ))}
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </ScrollView>
+      </Table>
+    </View>
+  );
+}
+
+export default TablePage;
